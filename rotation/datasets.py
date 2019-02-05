a055194ffd8bbbbc1296a57e451ab9cd3a611a2f
Facundo Quiroga <facundoq@gmail.com>
2019-02-05 13:51:24 -0300

    removed dep to keras.to_categorical

--- rotation/datasets.py
+++ rotation/datasets.py
@@ -9,7 +9,7 @@ names=["mnist","fashion_mnist","cifar10","mnist_rot","cluttered_mnist","lsa16","
 
 def get_data(dataset="mnist",dataformat="NHWC"):
     # the data, shuffled and split between train and test sets
-    if (dataset=="mnist"):
+    if dataset=="mnist":
         (x_train, y_train), (x_test, y_test) = mnist.load_data()
         x_train,x_test=np.expand_dims(x_train,axis=3),np.expand_dims(x_test, axis=3)
         img_channels,img_rows, img_cols = 1,28, 28
@@ -58,8 +58,6 @@ def get_data(dataset="mnist",dataformat="NHWC"):
     else:
         raise ValueError("Invalid channel format %s" % dataformat)
 
-
-    # Normalizacion
     
     # x_train /= x_train.max()
     # x_train -= x_train.mean(axis= (1,2), keepdims= 1)
@@ -67,8 +65,8 @@ def get_data(dataset="mnist",dataformat="NHWC"):
     # x_test -= x_test.mean(axis= (1,2), keepdims= 1)
 
     # convert class vectors to binary class matrices
-    y_train = keras.utils.to_categorical(y_train, num_classes)
-    y_test = keras.utils.to_categorical(y_test, num_classes)
+    y_train = to_categorical(y_train, num_classes)
+    y_test  = to_categorical(y_test, num_classes)
 
     # x_train=x_train.astype("float32")
     # x_test = x_test.astype("float32")
@@ -76,3 +74,35 @@ def get_data(dataset="mnist",dataformat="NHWC"):
     # y_test = y_test.astype("float32")
 
     return (x_train, y_train), (x_test, y_test), input_shape,num_classes,labels
+
+
+
+def to_categorical(y, num_classes=None, dtype='float32'):
+    """Converts a class vector (integers) to binary class matrix.
+
+    E.g. for use with categorical_crossentropy.
+
+    # Arguments
+        y: class vector to be converted into a matrix
+            (integers from 0 to num_classes).
+        num_classes: total number of classes.
+        dtype: The data type expected by the input, as a string
+            (`float32`, `float64`, `int32`...)
+
+    # Returns
+        A binary matrix representation of the input. The classes axis
+        is placed last.
+    """
+    y = np.array(y, dtype='int')
+    input_shape = y.shape
+    if input_shape and input_shape[-1] == 1 and len(input_shape) > 1:
+        input_shape = tuple(input_shape[:-1])
+    y = y.ravel()
+    if not num_classes:
+        num_classes = np.max(y) + 1
+    n = y.shape[0]
+    categorical = np.zeros((n, num_classes), dtype=dtype)
+    categorical[np.arange(n), y] = 1
+    output_shape = input_shape + (num_classes,)
+    categorical = np.reshape(categorical, output_shape)
+    return categorical
